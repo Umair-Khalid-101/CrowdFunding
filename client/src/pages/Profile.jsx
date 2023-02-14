38ccd847ac6186dbba216b7776c9a69b6a73ed0f
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-02-14 20:55:20 +0500

    Final

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+import { DisplayCampaigns } from "../components";
+import { useStateContext } from "../context";
+
+const Profile = () => {
+  const [loading, setLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getUserCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={loading}
+      campaigns={campaigns}
+    />
+  );
+};
+
+export default Profile;
